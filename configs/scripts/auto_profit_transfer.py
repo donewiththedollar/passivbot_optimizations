5eb0b06ae886bd09c982feddf450248c06686749
Tyler <donewiththedollar@gmail.com>
2022-11-02 00:00:58 -0400

    Auto profit transfer for scalper

--- configs/scripts/auto_profit_transfer.py
+++ configs/scripts/auto_profit_transfer.py
@@ -1,18 +1,33 @@
 import os
+import ta
+import ccxt
 if "NOJIT" not in os.environ:
     os.environ["NOJIT"] = "true"
-import warnings
 import traceback
 import argparse
 from njit_funcs import round_dynamic
 import logging
 import logging.config
+import pybit
 from pybit import usdt_perpetual
+from pybit.usdt_perpetual import HTTP
 from uuid import uuid4
 from config import *
 import time
 
-warnings.filterwarnings("ignore")
+exchange = ccxt.bybit(
+    {
+        'apiKey':api_key,
+        'secret':api_secret
+    }
+)
+
+ws_perp = usdt_perpetual.WebSocket(
+    test=False,
+    api_key=api_key,
+    api_secret=api_secret,
+    domain=domain
+)
 
 def main():
     logging.basicConfig(
@@ -32,21 +47,22 @@ def main():
      )
     args = parser.parse_args()
 
+
     try:
-        session = usdt_perpetual.HTTP(
+        session = HTTP(
             endpoint="https://api.bybit.com",
             api_key=api_key,
             api_secret=api_secret,
         )
     except:
-        logging.error(f"{args.user} failed to connect with Bybit")
+        logging.error(f"failed to connect with Bybit")
         logging.error(f"{traceback.format_exc()}")
         return
 
     try:
         usdt_balance = session.get_wallet_balance()['result']['USDT']['wallet_balance']
     except:
-        logging.error(f"{args.user} failed to get USDT balance")
+        logging.error(f"failed to get USDT balance")
         logging.error(f"{traceback.format_exc()}")
         return
 
@@ -54,7 +70,6 @@ def main():
 
     if amount_to_transfer < 1:
         logging.info(f"No extra funds to transfer. Current USDT balance: {usdt_balance}")
-        #notify_message(f"Passivbot:: No profits to transfer. Current USDT balance: {usdt_balance}")
         return
 
     logging.info(f"Transferring {amount_to_transfer} USDT to spot wallet. Old USDT balance: {usdt_balance},"
@@ -71,7 +86,7 @@ def main():
 
         logging.info(f"Transferring was successful. {amount_to_transfer} USDT was transferred to the spot wallet.")
     except:
-        logging.error(f"{args.user} failed to transfer funds")
+        logging.error(f"failed to transfer funds")
         logging.error(f"{traceback.format_exc()}")
         return
 
